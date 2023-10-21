0c3a171599b1faf5fc9b93c9cbaf22445f101cdc
Toukir Ahmed Lipto <134163593+DevLipto@users.noreply.github.com>
2023-10-21 11:33:00 +0600

    function.js

--- Assets/js/function.js
+++ Assets/js/function.js
@@ -55,3 +55,26 @@ $(document).ready(function () {
     content.style.display = "block";
   }, 900); // You can adjust the delay as needed
 });
+
+
+/** Scroll to top section Start **/
+
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+          $("#scrolltotop").fadeIn();
+      } else {
+          $("#scrolltotop").fadeOut();
+      }
+  });
+  $("#scrolltotop").click(function() {
+      $("html, body").animate({
+          scrollTop: 0
+      }, 0);
+  });
+});
+
+
+/** Scroll to top section End **/
+
